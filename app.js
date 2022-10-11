cf7bf3de4b5f894ed5c0473b15ba6d2531a7dc28
Krystal <krystalmai@gmail.com>
2022-10-11 11:11:18 +0700

    add error handling, query validation

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const indexRouter = require('./routes/index');
-const mongoose = require('mongoose');
-require('dotenv/config');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MONGODB
 mongoose.connect(process.env.MONGO_URI, () => {
-	console.log('Connected to Database!');
+  console.log("Connected to Database!");
 });
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+
+//catch when when request match no route
+app.use((req, res, next) => {
+  const exception = new Error(`Path not found`);
+  exception.statusCode = 404;
+  next(exception);
+});
+
+//customize express error handling middleware
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 400).send(err.message);
+});
 
 module.exports = app;
--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,73 +1,92 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
 const carController = {};
-const {sendResponse, AppError} = require("../helpers/utils")
+const { sendResponse, AppError } = require("../helpers/utils");
+const { query } = require("express");
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-		if (!req.body) throw new AppError(402, "Bad Request", "No request body")
-		const created = await Car.create(req.body)
-		sendResponse(res,200,true,{data:created},null,"Create Car Success")
-	} catch (err) {
-		// YOUR CODE HERE
-		next(err)
-	}
+  try {
+    // YOUR CODE HERE
+    if (!req.body) throw new AppError(402, "Bad Request", "No request body");
+    const created = await Car.create(req.body);
+    sendResponse(res, 200, true, { data: created }, null, "Create Car Success");
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-		const page = req.query.page || 1
-		// pagination
-		let offset = 20 * (page -1)
-		const { ...queryFilters } = req.query
-		queryFilters.isDeleted = false
-		const listOfCars = await Car.find(queryFilters)
-		let result = listOfCars.slice(offset, offset + 20)
-		let data = { cars: result, total: Math.ceil(listOfCars.length / 20) }
-		sendResponse(res, 200,true, data, null, "Found list of cars success")
-	} catch (err) {
-		// YOUR CODE HERE
-		next(err)
-	}
+  try {
+    // YOUR CODE HERE
+    const page = req.query.page || 1;
+    const allowedQueries = [
+      "make",
+      "model",
+      "release_date",
+      "transmission_type",
+      "size",
+      "style",
+      "price",
+    ];
+    // pagination
+    let offset = 20 * (page - 1);
+		const { ...queryFilters } = req.query;
+		const queryKeys = Object.keys(queryFilters)
+		queryKeys.forEach(key => {
+			if (!allowedQueries.includes(key)) {
+        throw new AppError(400, `Query ${key} is not allowed`, "Bad request" );
+			}
+			if (!queryFilters[key]) delete queryFilters[key];
+		})
+    queryFilters.isDeleted = false;
+    const listOfCars = await Car.find(queryFilters);
+    let result = listOfCars.slice(offset, offset + 20);
+    let data = { cars: result, total: Math.ceil(listOfCars.length / 20) };
+    sendResponse(res, 200, true, data, null, "Found list of cars success");
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	
-	try {
-		if (!req.body || !req.params.id) throw new AppError(402, "Bad Request", "No request body or no car id");
+  try {
+    if (!req.body || !req.params.id)
+      throw new AppError(400, "No request body or no car id", "Bad Request");
 
-		const { id } = req.params;
-		const updateInfo = req.body;
-		const options = { new: true };
+    const { id } = req.params;
+    const updateInfo = req.body;
+    const options = { new: true };
 
-		const updated = await Car.findByIdAndUpdate(id, updateInfo, options);
-		sendResponse(res, 200, true, {data: updated}, null, "Update car success")
+    const updated = await Car.findByIdAndUpdate(id, updateInfo, options);
+    sendResponse(res, 200, true, { data: updated }, null, "Update car success");
 
-
-		// YOUR CODE HERE
-		
-	} catch (err) {
-		// YOUR CODE HERE
-		next(err)
-	}
+    // YOUR CODE HERE
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-		if (!req.params.id) throw new AppError(402, "Bad Request", "No car id");
+  try {
+    // YOUR CODE HERE
+    if (!req.params.id) throw new AppError(400,  "No car id", "Bad Request",);
 
-		const { id } = req.params;
-		const options = { new: true };
+    const { id } = req.params;
+    const options = { new: true };
 
-		const updated = await Car.findByIdAndUpdate(id, { isDeleted: true }, options) //soft delete
-		sendResponse(res, 200, true, {data: updated}, null, "Delete car success")
-	} catch (err) {
-		// YOUR CODE HERE
-		next(err)
-	}
+    const updated = await Car.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      options
+    ); //soft delete
+    sendResponse(res, 200, true, { data: updated }, null, "Delete car success");
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 module.exports = carController;
